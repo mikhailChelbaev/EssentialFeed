80a1512f997fcab2e01c3b6a6b9a7fcb3b123fe8
Mikhail <chelbaev.mihail70@gmail.com>
2022-02-12 15:13:16 +0300

    From Singletons and Globals to Proper Dependency Injection

--- EssentialFeed/FeadFeature/FeedLoader.swift
+++ EssentialFeed/FeadFeature/FeedLoader.swift
@@ -15,3 +15,21 @@ enum LoadFeedResult {
 protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
+
+class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
