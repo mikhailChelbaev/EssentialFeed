3484d87305b03ddc9b9e0d6a9e0c6061d43c2445
Mikhail <chelbaev.mihail70@gmail.com>
2022-03-04 18:02:38 +0300

    URLCache as a Persistence Alternative & Solving The Infamous “But it works on my machine!” Caching Problem

--- EssentialFeed/Fead API/FeedItemsMapper.swift
+++ EssentialFeed/Fead API/FeedItemsMapper.swift
@@ -31,9 +31,8 @@ internal final class FeedItemsMapper {
     private static var OK_200: Int { return 200 }
     
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == OK_200,
-                let root = try? JSONDecoder().decode(Root.self, from: data) else {
-                    return .failure(RemoteFeedLoader.Error.invalidData)
+        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(RemoteFeedLoader.Error.invalidData)
         }
         
         return .success(root.feed)
