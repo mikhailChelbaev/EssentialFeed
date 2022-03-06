0b30f58aefc093941917f952d1dc3f5a9be4cacc
Mikhail <chelbaev.mihail70@gmail.com>
2022-03-06 15:10:52 +0300

    Proper Memory-Management of Captured References Within Deeply Nested Closures + Identifying Highly-Coupled Modules

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Mikhail on 06.03.2022.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
