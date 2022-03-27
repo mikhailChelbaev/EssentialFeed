7896ee062115f99539a8f88a4e8c3e76f0c7591b
Mikhail <chelbaev.mihail70@gmail.com>
2022-03-27 21:38:50 +0300

    Gathering Fast Feedback and Validating UI Design and Dev Decisions Through Realistic App Prototypes

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,48 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Mikhail on 27.03.2022.
+//
+
+import UIKit
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
+class FeedViewController: UITableViewController {
+    private let feed = FeedImageViewModel.prototypeFeed
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source & delegate
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feed.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.item]
+        cell.configure(with: model)
+        return cell
+    }
+}
+
+extension FeedImageCell {
+    func configure(with model: FeedImageViewModel) {
+        locationLabel.text = model.location
+        locationContainer.isHidden = model.location == nil
+        
+        descriptionLabel.text = model.description
+        descriptionLabel.isHidden = model.description == nil
+        
+        fadeIn(UIImage(named: model.imageName))
+    }
+}
