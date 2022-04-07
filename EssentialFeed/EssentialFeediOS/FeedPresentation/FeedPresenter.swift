cc51be577b6574fc9fa70cf18dfb563395348fb1
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-07 12:08:21 +0300

    Creating, Localizing, and Testing Customer Facing Strings in the Presentation Layer + NSLocalizedString Best Practices

--- EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Mikhail on 06.04.2022.
 //
 
+import Foundation
 import EssentialFeed
 
 protocol FeedLoadingView {
@@ -22,6 +23,14 @@ final class FeedPresenter {
     init(feedView: FeedView, loadingView: FeedLoadingView) {
         self.feedView = feedView
         self.loadingView = loadingView
+    }
+    
+    static var title: String {
+        return NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view")
     }
     
     func didStartLoadingFeed() {
