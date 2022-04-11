3ec5157a10db99cd144e0e64d0e68fa81779affb
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-11 13:47:15 +0300

    Test-driven Approach to Effectively Dealing with Legacy Code (Code With No Tests!) + Extracting Cross-platform Components From a Platform-specific Module

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -5,8 +5,6 @@
 //  Created by Mikhail on 06.04.2022.
 //
 
-import EssentialFeed
-
-struct FeedViewModel {
-    let feed: [FeedImage]
+public struct FeedViewModel {
+    public let feed: [FeedImage]
 }
