3a8d204d27b82a92d412f7377596ccb8bee0177f
Mikhail <chelbaev.mihail70@gmail.com>
2022-04-16 00:37:08 +0300

    Add new FeedStore core data model version adding an optional `Data` property to the `ManagedFeedImage`

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,16 +8,17 @@
 import CoreData
 
 @objc(ManagedFeedImage)
- class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged  var id: UUID
     @NSManaged  var imageDescription: String?
     @NSManaged  var location: String?
     @NSManaged  var url: URL
+    @NSManaged var data: Data?
     @NSManaged  var cache: ManagedCache
 }
 
 extension ManagedFeedImage {
-     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
@@ -28,7 +29,7 @@ extension ManagedFeedImage {
         })
     }
     
-     var local: LocalFeedImage {
+    var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
 }
