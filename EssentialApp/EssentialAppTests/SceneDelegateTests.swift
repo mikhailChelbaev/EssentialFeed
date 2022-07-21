0d1854f85b1a40c64b0b8b6eb433efa6e5522ff3
Mikhail <chelbaev.mihail70@gmail.com>
2022-07-22 00:34:20 +0300

    fix window configuration test

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -12,14 +12,13 @@ import EssentialFeediOS
 class SceneDelegateTests: XCTestCase {
   
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        let window = UIWindow()
+        let window = UIWindowSpy()
         let sut = SceneDelegate()
         sut.window = window
         
         sut.configureWindow()
         
-//        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
-        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected window to make window key and visible")
     }
     
     func test_configureWindow_configuresRootViewController() {
@@ -34,6 +33,16 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as a top controller, got \(String(describing: topController)) instead")
+    }
+    
+    // MARK: - Helpers
+    
+    private class UIWindowSpy: UIWindow {
+        var makeKeyAndVisibleCallCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount = 1
+        }
     }
     
 }
