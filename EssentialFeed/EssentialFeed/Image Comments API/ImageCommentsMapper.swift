b48686af8004f78da6e4a9065b53e9bd7213f9ae
Mikhail <chelbaev.mihail70@gmail.com>
2022-07-30 00:23:42 +0300

    Test ImageCommentsMapper in isolation

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -27,7 +27,7 @@ public final class ImageCommentsMapper {
         }
     }
     
-    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         
