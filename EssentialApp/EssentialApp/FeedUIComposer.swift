f26961901bd9f91086449ee04388c9a6cf5c4c8d
Mikhail <chelbaev.mihail70@gmail.com>
2022-09-10 00:24:31 +0300

    Show comments on image selection

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -18,7 +18,7 @@ public final class FeedUIComposer {
     public static func feedComposedWith(
         feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
-        selection: @escaping (FeedImage) -> Void = { _ in }
+        selection: @escaping (FeedImage) -> Void
     ) -> ListViewController {
         let presentationAdapter = FeedPresentationAdapter(
             loader: feedLoader)
