f800b1db6b9fb8f7ec53f5d4546907d21d26d75c
Mikhail <chelbaev.mihail70@gmail.com>
2022-09-18 16:31:45 +0300

    Add default presentation mapper when the Resource type matches the ResourceViewModel type

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -28,6 +28,13 @@ public class LoadResourcePresenter<Resource, View: ResourceView> {
         self.mapper = mapper
     }
     
+    public init(resourceView: View, loadingView: ResourceLoadingView, errorView: ResourceErrorView) where Resource == View.ResourceViewModel {
+        self.resourceView = resourceView
+        self.loadingView = loadingView
+        self.errorView = errorView
+        self.mapper = { $0 }
+    }
+    
     public static var loadError: String {
         return NSLocalizedString(
             "GENERIC_CONNECTION_ERROR",
